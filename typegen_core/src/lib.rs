ceb9b44af5ab232972a3f5e5d69b7f478bba25ce
Erik Vesteraas <erik@vestera.as>
2021-03-26 11:13:58 +0100

    Temp commit

--- typegen_core/src/lib.rs
+++ typegen_core/src/lib.rs
@@ -1,6 +1,6 @@
-use std::io::Read;
-use crate::shape::{Shape, shape_to_string};
 use crate::jsoninfer::FromJson;
+use crate::shape::{shape_to_string, Shape};
+use std::io::Read;
 
 mod jsoninfer;
 mod jsoninputerr;
@@ -11,3 +11,25 @@ pub fn typegen(input: impl Read) -> String {
     let shape = Shape::from_json(input).unwrap();
     shape_to_string(&shape)
 }
+
+//
+// fn get_sample<'a>(source: &'a SampleSource) -> Result<Box<dyn Read + 'a>, JTError> {
+//     let sample: Box<dyn Read> = match *source {
+//         #[cfg(feature = "remote-samples")]
+//         SampleSource::Url(url) => Box::new(reqwest::get(url)?),
+//         #[cfg(not(feature = "remote-samples"))]
+//         SampleSource::Url(_) => {
+//             return Err("Remote samples disabled".into());
+//         }
+//
+//         #[cfg(feature = "local-samples")]
+//         SampleSource::File(path) => Box::new(File::open(path)?),
+//         #[cfg(not(feature = "local-samples"))]
+//         SampleSource::File(_) => {
+//             return Err("Local samples disabled".into());
+//         }
+//
+//         SampleSource::Text(text) => Box::new(text.as_bytes()),
+//     };
+//     Ok(sample)
+// }
